bed8beeca1ddab88b6da1b96f9e18005d57ec7e6
conluiben <conradoluizbencio@gmail.com>
2023-04-14 23:43:18 +0800

    Register Page Styles Attempt (WIP)
    - attempted to revamp styles by deactivating vuetify and writing CSS from scratch
    - added separate component Header.vue for navbar in all pages

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <div class="navHeader">
+        My First Vue: Tab Tracker!
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navHeader{
+    /* box-sizing: border-box; */
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 15px 10px;
+    /* font-family: "Open Sans"; */
+    background-color: lightblue;
+    width: 100%;
+    color: whitesmoke;
+}
+
+</style>

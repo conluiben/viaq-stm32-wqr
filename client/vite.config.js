0aa635ff36dc940808fd9806f41c1dd197b6870f
conluiben <conradoluizbencio@gmail.com>
2023-06-23 13:59:50 +0800

    Frontend Attempt (broken)
    - attempt to make frontend deployment work (but still not working with "railway up" command!) Works with railway run npm run preview though...

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// ? configuring Network hosting with Vite: (see forum for correct vite command)
+//// ? https://github.com/vitejs/vite/discussions/3396
+// ? it must be npm run dev -- --host OR vite --host 
+
+const ASSET_URL = process.env.ASSET_URL || '';
+
+//learned from https://vitejs.dev/config/#conditional-config
+//https://stackoverflow.com/questions/68076527/how-to-set-vite-config-js-base-public-path
+//! not used; will leave here for future reference
+/*
+export default defineConfig(({command,serve,ssrBuild}) => {
+  if(command==="serve"){ // or dev
+
+  }
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+*/
+
 export default defineConfig({
   plugins: [vue()],
+  // base: '/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
